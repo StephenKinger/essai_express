00afed9050a826a1e382b5deecd900bc9043f149
Stephen <stephen.kinger@gmail.com>
2016-09-23 18:30:31 +0200

    Connexion à une Base de données mongodb

--- server.js
+++ server.js
@@ -3,25 +3,46 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient
-  .connect('mongodb://steph:test@ds033966.mlab.com:33966/letthewookiewin' ,
-  (err, database) => {
-    console.log('err' + err);
-    console.log('database' + database);
-  })
+var db;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+
+MongoClient
+  .connect('mongodb://steph:test@ds033966.mlab.com:33966/letthewookiewin' ,
+  (err, database) => {
+    if (err) {
+      return concole.log(err);
+    }
+    else {
+      db = database;
+      app.listen(3000, () => {
+        console.log("listening on 3000");
+      })
+    }
+})
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  var cursor = db.collection('quotes').find().toArray((err, results) => {
+    console.log('get');
+    console.log(results);
+    res.render('index.ejs', {quotes: results});
+  })
+
+  //res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
-
-app.listen(3000, function() {
-  console.log("listening on 3000")
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    else {
+      console.log('saved to database');
+      res.redirect('/');
+    }
+  })
 })
